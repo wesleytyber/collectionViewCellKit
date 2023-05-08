c38fb932dad884e07f299b424d1a3960728995f4
wesley.santos <wesley.santos@brave.ag>
2023-05-08 18:33:50 -0300

    feat: [master] - Added modular components

---
+++ Sources/CollectionViewCellKit/Model/Model.swift
@@ -0,0 +1,14 @@
+//
+//  UserInfos.swift
+//
+//
+//  Created by Wesley Calazans on 08/05/23.
+//
+
+import Foundation
+
+public struct UserInfos: Codable {
+    public var accountType, currencyBalanceLabel, flag, balance: String
+    public var actionLabel, actionLabelColor, actionBackground, colorIcon: String
+}
+
